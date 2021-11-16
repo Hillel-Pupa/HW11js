949dbfed43927759b298d61686ceed3ce3aff74c
Anna <protsenko.anyta@gmail.com>
2021-11-16 21:48:45 +0100

    show users

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-import { showUsers } from "./function.js";
+import { userListOnClick, showUsers, moreInfo } from "./function.js";
 import { users } from "./users.js";
+import { userList } from "./function.js";
+import { userInfo } from "./function.js";
+import { userTemplate } from "./function.js";
 
 for (let index = 0; index < users.length; index++) {
   showUsers();
+  moreInfo();
 }
--- js/function.js
+++ js/function.js
@@ -1,8 +1,24 @@
 "use strict";
 import { users } from "./users.js";
-const userList = document.querySelector(".user-list");
-const userInfo = document.querySelector(".user-info");
-const userTemplate = document.querySelector("#user-content");
+export const userList = document.querySelector(".user-list");
+export const userInfo = document.querySelector(".user-info");
+export const userTemplate = document.querySelector("#user-content");
+const moreInfoTamplate = document.querySelector("#more-info");
+
+export function moreInfo() {
+  const newMoreInfoTamplate = moreInfoTamplate.content.cloneNode(true);
+  userInfo.appendChild(newMoreInfoTamplate);
+  for (let index = 0; index < users.length; index++) {
+    const userView = document.querySelector(".user-view");
+    const age = document.querySelector(".age");
+    const gender = document.querySelector(".gender");
+    userView.appendChild(age);
+    userView.appendChild(gender);
+    userInfo.appendChild(userView);
+    age.textContent = `age: ${users[index].age}`;
+    gender.textContent = `gender: ${users[index].gender}`;
+  }
+}
 
 export function User(name, surname, age, gender) {
   this.name = name;
@@ -14,12 +30,29 @@ export function User(name, surname, age, gender) {
 export function showUsers() {
   const newTemplate = userTemplate.content.cloneNode(true);
   userList.appendChild(newTemplate);
-  const user = document.querySelector(".user");
-  const userName = document.querySelector(".name");
-  const userSurname = document.querySelector(".surname");
   for (let index = 0; index < users.length; index++) {
-    userName.textContent = users[0].name;
+    const user = document.querySelector(".user");
+    const userName = document.querySelector(".name");
+    const userSurname = document.querySelector(".surname");
+    user.appendChild(userName);
+    user.appendChild(userSurname);
+    userList.appendChild(user);
+    userName.textContent = users[index].name;
+    userSurname.textContent = users[index].surname;
   }
 }
+
+export function userListOnClick(event) {
+  // console.log(event);
+  // return;
+  if (event.target.classList.contains("delete")) {
+    const user = event.target.parentElement;
+    user.remove();
+  }
+  if (event.target.classList.contains("view")) {
+  }
+}
+
+export function showMoreInfo() {}
 
 // console.log(users[0].name);
