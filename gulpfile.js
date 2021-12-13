909f8738e2719b74f4f5baf7b61b5e8eabfa2076
Anna <protsenko.anyta@gmail.com>
2021-12-13 16:39:57 +0100

    add

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,12 @@ const uglify = require("gulp-uglify");
 const babel = require("gulp-babel");
 const concat = require("gulp-concat");
 const sass = require("gulp-sass")(require("sass"));
-const linter = require("gulp-eslint");
 
 gulp.task("js", () => {
   return gulp
     .src("js/*.js")
     .pipe(babel())
-    .pipe(linter())
-    .pipe(linter.format())
+
     .pipe(uglify())
     .pipe(concat("build.js"))
     .pipe(gulp.dest("build"));
